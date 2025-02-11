115eef66e7793d57abd3a9e38b0ca3cb0c7f976a
MARKUVPC\marek <marek.kanok@seznam.cz>
2025-02-12 00:09:24 +0100

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LeaveManagementSystem.Web.Models;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModul
+            {
+                Name = "Student of MVC mastery",
+                DateOfBirth = new DateTime(1954, 01, 27)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModul.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModul
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
